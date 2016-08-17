43a5bbd3ff50fdaee68793966249b3fd434a8bd5
anton <office@ajantonov.com>
2016-08-17 22:23:01 +0300

    Invalid usage of underscore in literals.

---
+++ primitives/InvalidUnderscore.java
@@ -0,0 +1,11 @@
+public class InvalidUnderscore {
+	
+	double notAtStart = _1000.00;
+	double notAtEnd = 1000.00_;
+	double notByDecimal = 1000_.00;
+	double annoyingButLegal = 1_00_0.0_0;
+
+	public static void main(String[] args){
+		
+	}
+}
