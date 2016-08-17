88216d69788e8a300426710c848824e962a338fe
anton <office@ajantonov.com>
2016-08-17 12:20:08 +0300

    Added two examples of creating package.

---
+++ package/packagec/ClassC.java
@@ -0,0 +1,4 @@
+package packagec;
+
+public class ClassC {
+}
---
+++ package/packaged/ClassD.java
@@ -0,0 +1,11 @@
+package packaged;
+
+import packagec.ClassC;
+
+public class ClassD {
+	ClassC c;
+
+	public static void main(String[] args) {
+		System.out.println("Gooot it!");
+	}
+}	
