a5914a542ea13cb967f59a946aeee7d65adda457
Anton Antonov <ajantonov@gmail.com>
2016-08-19 15:23:31 +0300

    Add example for declaring multiple variables.

---
+++ reference/DeclaringMultipleVariables.java
@@ -0,0 +1,8 @@
+public class DeclaringMultipleVariables {
+	String s1, s2;
+	String s3 = "yes", s4 = "no";
+
+	public void main(String[] args){
+		DeclaringMultipleVariables d = new DeclaringMultipleVariables();
+	}
+}
