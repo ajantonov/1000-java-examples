91a6e224a8c76c2f3245d617b920c2fe4f0506e3
Anton Antonov <ajantonov@gmail.com>
2016-08-19 15:19:35 +0300

    Add example for declararing and initialiazing variables.

---
+++ reference/DeclaringAndInitializingVariables.java
@@ -0,0 +1,14 @@
+public class DeclaringAndInitializingVariables {
+	String	zooName;
+	int	numberAnimals;
+
+	DeclaringAndInitializingVariables() {
+		zooName = "The Best Zoo";
+		numberAnimals = 100;
+	}
+
+	public static void main(String[] args){
+		DeclaringAndInitializingVariables d = new DeclaringAndInitializingVariables();
+	}
+
+}
