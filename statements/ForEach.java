e796ce0d4c377c7464ea05b5aca74685a0fb78dd
Anton Antonov <ajantonov@gmail.com>
2016-08-22 12:49:19 +0300

    Add other example with java.util.List<String>.

--- statements/ForEach.java
+++ statements/ForEach.java
@@ -1,12 +1,19 @@
 public class ForEach {
     
     public static void main(String[] args) {
-        final String[] names = new String[3];
-        names[0] = "Lisa";
-        names[1] = "Kevin";
-        names[2] = "Roger";
-        for(String name : names) {
-            System.out.print(name + ", ");
+        // final String[] names = new String[3];
+        // names[0] = "Lisa";
+        // names[1] = "Kevin";
+        // names[2] = "Roger";
+        // for(String name : names) {
+        //    System.out.print(name + ", ");
+        // }
+        java.util.List<String> values = new java.util.ArrayList<String>();
+        values.add("Lisa");
+        values.add("Kevin");
+        values.add("Roger");
+        for(String value : values) {
+            System.out.print(value + ", ");
         }
     }
     
