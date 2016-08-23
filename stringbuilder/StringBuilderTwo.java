834d828a79e545ef1b52a8df4f8bb09184feb603
Anton Antonov <ajantonov@gmail.com>
2016-08-23 23:51:25 +0300

    Added example for mutability of StringBuilder.

---
+++ stringbuilder/StringBuilderTwo.java
@@ -0,0 +1,12 @@
+public class StringBuilderTwo {
+    public static void main(String[] args){
+        // StringBuilder sb = new StringBuilder("start");
+        // sb.append("+middle");
+        // StringBuilder same = sb.append("+end");
+        StringBuilder a = new StringBuilder("abc");
+        StringBuilder b = a.append("de");
+        b = b.append("f").append("g");
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+    }
+}
