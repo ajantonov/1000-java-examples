476c95fb156b45b03aeaec5e26ea839c5291c4ac
Anton Antonov <ajantonov@gmail.com>
2016-08-23 16:19:32 +0300

    Add example for method charAt(position) of String class.

---
+++ CharAt.java
@@ -0,0 +1,8 @@
+public class CharAt {
+    public static void main(String[] args) {
+        String string = "animals";
+        string.charAt(0);
+        string.charAt(6);
+        string.charAt(7);
+    }
+}
