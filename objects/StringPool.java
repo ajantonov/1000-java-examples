feee7313876b37deb1c7f911a92d4eefcab8707c
Anton Antonov <ajantonov@gmail.com>
2016-08-23 16:11:06 +0300

    Add example for string pool.

---
+++ objects/StringPool.java
@@ -0,0 +1,8 @@
+public class StringPool {
+    public static void main(String[] args){
+        // String pool usage.
+        String name = "Fluffy";
+        // String JVM usage.
+        String otherName = new String("Fluffy");
+    }
+}
