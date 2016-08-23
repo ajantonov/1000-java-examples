379f96b040e47b7a0b5060438c7ee75a089796b7
Anton Antonov <ajantonov@gmail.com>
2016-08-23 17:40:22 +0300

    Add examples for methods toUpperCase() and toLowerCase() for String class.

---
+++ string/ToLowerToUpperCase.java
@@ -0,0 +1,8 @@
+public class ToLowerToUpperCase {
+
+    public static void main(String[] args){
+        String string = "animals";
+        System.out.println(string.toUpperCase());
+        System.out.println("Abc123".toUpperCase());
+    }
+}
