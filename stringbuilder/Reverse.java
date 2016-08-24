73e54df06d4d1d0758c27d738851530516db172b
Anton Antonov <ajantonov@gmail.com>
2016-08-24 13:37:18 +0300

    Add example for method reverse() of StringBuilder class.

---
+++ stringbuilder/Reverse.java
@@ -0,0 +1,7 @@
+public class Reverse {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("ABC");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
