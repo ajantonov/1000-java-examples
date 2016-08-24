5c556bb8878710205129edbf5f0408442406fe6e
Anton Antonov <ajantonov@gmail.com>
2016-08-24 13:32:44 +0300

    Add examples for methods delete() and deleteCharAt() of StringBuilder class.

---
+++ stringbuilder/DeleteDeleteCharAt.java
@@ -0,0 +1,7 @@
+public class DeleteDeleteCharAt {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("abcdef");
+        sb.delete(1, 3);
+        sb.deleteCharAt(77);
+    }
+}
