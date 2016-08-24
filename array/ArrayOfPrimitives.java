46bf3077f22d36b33cd248870ebf8225ef7d5b5b
ajantonov <ajantonov@yahoo.com>
2016-08-24 17:12:50 +0300

    Add example for array of primitive types.

---
+++ array/ArrayOfPrimitives.java
@@ -0,0 +1,11 @@
+public class ArrayOfPrimitives {
+    public static void main(String[] args){
+        int[] numbers = new int[3];
+        int[] numbers2 = new int[]{42, 55, 99};
+        int[] numbers3 = {42, 55, 99}; 
+        int[] numAnimals;
+        int [] numAnimals2;
+        int numAnimals3[];
+        int numAnimals4[];
+    } 
+}
