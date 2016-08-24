ad0b9de7083b52d79f8ef7a5547c501fd2431f75
ajantonov <ajantonov@yahoo.com>
2016-08-24 13:48:04 +0300

    Add example for method toString() of class StringBuilder.

---
+++ stringbuilder/ToString.java
@@ -0,0 +1,6 @@
+public class ToString {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Anton Jorov Antonov");
+        System.out.println(sb.toString());
+    }
+}
