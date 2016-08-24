9c0ef1b04e22cf5fc1d33f9c3285224fc10f15d0
ajantonov <ajantonov@yahoo.com>
2016-08-24 15:56:45 +0300

    Add example for unequality of String objects instantiated in a different way.

---
+++ stringbuilder/StringPoolUnEqualityTwo.java
@@ -0,0 +1,7 @@
+public class StringPoolUnEqualityTwo{
+    public static void main(String[] args){
+        String x = new String("Hello World");
+        String y = "Hello World";
+        System.out.println(x == y);
+    }
+}
