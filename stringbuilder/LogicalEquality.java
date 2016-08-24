bdc974b9c5301805130461e290605d070d595d60
ajantonov <ajantonov@yahoo.com>
2016-08-24 16:14:42 +0300

    Add example of logical equality between two String objects.

---
+++ stringbuilder/LogicalEquality.java
@@ -0,0 +1,7 @@
+public class LogicalEquality {
+    public static void main(String[] args){
+        String x = "Hello World";
+        String z = " Hello World".trim();
+        System.out.println(x.equals(z));
+    }
+}
