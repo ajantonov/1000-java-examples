3684d33aea063bb81ffbd58208cf8083f5e23636
ajantonov <ajantonov@yahoo.com>
2016-08-24 18:01:27 +0300

    Add example for casting array types.

---
+++ array/CastingArrayType.java
@@ -0,0 +1,9 @@
+public class CastingArrayType {
+    public static void main(String[] args){
+        String[] strings = {"stringValue"};
+        Object[] objects = strings;
+        String[] againStrings = (String[])objects;
+        againStrings[0] = new StringBuilder();
+        objects[0] = new StringBuilder(); 
+    }
+}
