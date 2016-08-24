9b663d4f3c1d48a2ddf57f7efbb5ac38ed4b4813
ajantonov <ajantonov@yahoo.com>
2016-08-24 18:06:48 +0300

    Add example for usage of array.

---
+++ array/UsingArray.java
@@ -0,0 +1,9 @@
+public class UsingArray {
+    public static void main(String[] args){
+        String[] mammals = { "monkey", "chimp", "donkey" };
+        System.out.println(mammals.length);
+        System.out.println(mammals[0]);
+        System.out.println(mammals[1]);
+        System.out.println(mammals[2]);
+    }
+}
