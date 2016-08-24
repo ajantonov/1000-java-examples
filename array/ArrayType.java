71aea96d0f7d76404b9352128b3b70a0a7406241
ajantonov <ajantonov@yahoo.com>
2016-08-24 17:31:50 +0300

    Add a line for appropriate array formating

--- array/ArrayType.java
+++ array/ArrayType.java
@@ -3,6 +3,6 @@ public class ArrayType {
         String[] bugs = { "cricket", "beetle", "ladybug" };
         String[] alias = bugs;
         System.out.println(bugs.equals(alias));
-        System.out.println(bugs.toString());
+        System.out.println(java.util.Arrays.toString(bugs));
     }
 }
