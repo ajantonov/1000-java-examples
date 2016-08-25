8cd7de01c9473b08b302a8a7172dd3b2529abdc3
ajantonov <ajantonov@yahoo.com>
2016-08-25 18:22:52 +0300

    Add example for method add() of ArrayList.

---
+++ array-list/Add.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Add {
+    public static void main(String...args){
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("hawk");
+        list.add(Boolean.TRUE);
+        System.out.println(list);
+    }
+}
