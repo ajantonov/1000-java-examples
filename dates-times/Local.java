a54ca5c05f42069151a8d968e601af4d5fe17947
ajantonov <ajantonov@yahoo.com>
2016-08-26 17:34:01 +0300

    Add example for simple usage of LocalDate, LocalTime, and LocalDateTime.

---
+++ dates-times/Local.java
@@ -0,0 +1,9 @@
+import java.time.*;
+
+public class Local {
+    public static void main(String[] args){
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());        
+    }
+}
