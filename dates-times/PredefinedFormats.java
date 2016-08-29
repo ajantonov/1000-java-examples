704f7acd89d4f1b0ab930ca6ff9d886effa898e8
ajantonov <ajantonov@yahoo.com>
2016-08-29 15:12:34 +0300

    Add own created date time format.

--- dates-times/PredefinedFormats.java
+++ dates-times/PredefinedFormats.java
@@ -8,11 +8,13 @@ public class PredefinedFormats {
         LocalDate date = LocalDate.of(2020, Month.JANUARY, 1);
         LocalDateTime dateTime = LocalDateTime.of(date, time);
         
-        DateTimeFormatter shortFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
-        DateTimeFormatter mediumFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        DateTimeFormatter shortFormatter    = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        DateTimeFormatter mediumFormatter   = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        DateTimeFormatter own               = DateTimeFormatter.ofPattern("MMMM dd, yyyy, hh:mm");
         
         System.out.println(dateTime.format(shortFormatter));
         System.out.println(dateTime.format(mediumFormatter));
+        System.out.println(dateTime.format(own));
  
     }
 }
