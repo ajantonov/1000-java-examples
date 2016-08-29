e105f09693c50582a9b8fece6cbca7be5fb12796
ajantonov <ajantonov@yahoo.com>
2016-08-29 15:06:08 +0300

    Add examples for predefined formats of DateTimeFormatter class.

---
+++ dates-times/PredefinedFormats.java
@@ -0,0 +1,18 @@
+import java.time.*;
+import java.time.format.*;
+
+public class PredefinedFormats {
+    
+    public static void main(String[] args){
+        LocalTime time = LocalTime.of(11, 12, 34);
+        LocalDate date = LocalDate.of(2020, Month.JANUARY, 1);
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        
+        DateTimeFormatter shortFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        DateTimeFormatter mediumFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        
+        System.out.println(dateTime.format(shortFormatter));
+        System.out.println(dateTime.format(mediumFormatter));
+ 
+    }
+}
