6f434c8675c75016f99733a66e9b7effc2a17539
ajantonov <ajantonov@yahoo.com>
2016-09-02 17:13:43 +0300

    Add example for object instances counting with static member.

---
+++ method/static/Counter.java
@@ -0,0 +1,17 @@
+public class Counter {
+    
+    private static int count = 0;
+    
+    public Counter() {
+        count++;
+    }
+    
+    public static void main(String[] args) {
+        Counter counter1 = new Counter();
+        Counter counter2 = new Counter();
+        Counter counter3 = new Counter();
+        Counter counter4 = new Counter();
+        Counter counter5 = new Counter();
+        System.out.println(Counter.count);
+    }
+}
