a8716e74b4ee0fc9cdbfc2b78fed5f5119cf2fe5
ajantonov <ajantonov@yahoo.com>
2016-09-02 17:39:36 +0300

    Add example for final static reference.

---
+++ variables/static/FinalReference.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class FinalReference {
+    
+    private static final ArrayList<String> finalReference = new ArrayList<>();
+
+    public static void main(String[] args) {
+        finalReference.add("changed");        
+    }
+}
