46d8453d6b8e60ef0acc10bb5e18727c06c321b9
ajantonov <ajantonov@yahoo.com>
2016-09-02 23:24:13 +0300

    Add example for importing static members.

---
+++ imports/static/StaticImport.java
@@ -0,0 +1,8 @@
+import static java.util.Arrays.asList;
+import static java.util.Arrays.asList;
+
+public class StaticImport {
+    public static void main(String[] args) {
+       asList("one");
+    }
+}
