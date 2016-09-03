c6eac950218a814d68b890e9949bfe347d4569b6
ajantonov <ajantonov@yahoo.com>
2016-09-03 13:42:01 +0300

    Add example for two type conversions of parameter type in overloaded method.

---
+++ method/overloading/TooManyConversions.java
@@ -0,0 +1,12 @@
+public class TooManyConversions {
+    public static void play(Long l) {
+    }
+    
+    public static void play(Long... l) {
+    }
+
+    public static void main(String[] args) {
+        play(4);
+        play(4L);
+    }
+}
