bc13eef907935a8fdf8a52f7817e181deae0a63b
ajantonov <ajantonov@yahoo.com>
2016-09-03 13:07:35 +0300

    Add example for method overloading and type matching.

---
+++ method/overloading/ReferenceTypes.java
@@ -0,0 +1,16 @@
+public class ReferenceTypes {
+
+    public void fly(String s) {
+        System.out.println("string");
+    }
+
+    public void fly(Object o) {
+        System.out.println("object");
+    }
+
+    public static void main(String[] args) {
+        ReferenceTypes r = new ReferenceTypes();
+        r.fly("test");
+        r.fly(56);
+    }
+}
