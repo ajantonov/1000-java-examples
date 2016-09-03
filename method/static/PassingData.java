9d0a5ff64ea48e744f53d9b0d205a62814673124
ajantonov <ajantonov@yahoo.com>
2016-09-03 12:08:37 +0300

    Add example for passing data among methods.

---
+++ method/static/PassingData.java
@@ -0,0 +1,12 @@
+public class PassingData {
+    
+    public static void main(String[] args) {
+        int num = 4;
+        newNumber(5);
+        System.out.println(num);
+    }
+    
+    public static void newNumber(int num) {
+        num = 8;
+    }
+}
