bbb59c92fda696b8c0eb8566a2c350a2ff424787
ajantonov <ajantonov@yahoo.com>
2016-09-03 16:13:20 +0300

    Add example for constructor with parameters.

--- constructor/Bunny.java
+++ constructor/Bunny.java
@@ -1,9 +1,16 @@
 public class Bunny {
+    
+    private String color;
+
+    public Bunny(String color) {
+        this.color = color;
+    }
+
     public Bunny() {
         System.out.println("Constructor");
     }
 
     public static void main(String[] args) {
-        Bunny b = new Bunny();
+        Bunny b = new Bunny("Соросинкяво");
     }
 }
