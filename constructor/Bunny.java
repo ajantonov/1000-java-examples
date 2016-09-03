c4d2a3f4b2a1ae316a8cd08c462edc32644a500e
ajantonov <ajantonov@yahoo.com>
2016-09-03 16:35:29 +0300

    Add example for constructor with inappropriate member initialization.

--- constructor/Bunny.java
+++ constructor/Bunny.java
@@ -1,16 +1,26 @@
 public class Bunny {
     
-    private String color;
-
-    public Bunny(String color) {
-        this.color = color;
-    }
+    private String  color;
+    private int     length;
+    private int     height;
 
     public Bunny() {
         System.out.println("Constructor");
     }
 
+    public Bunny(String color) {
+        this.color = color;
+    }
+
+    public Bunny(int length, int theHeight) {
+        length = this.length;
+        this.height = theHeight;
+        this.color = "white";
+    }
+
     public static void main(String[] args) {
-        Bunny b = new Bunny("Соросинкяво");
+        // Bunny b = new Bunny("Соросинкяво");
+        Bunny b = new Bunny(1, 2);
+        System.out.println(b.length + " " + b.height + " " + b.color);
     }
 }
