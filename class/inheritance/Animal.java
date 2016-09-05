01ad307a9ea7cc06eabc50877acde719f1035bc4
ajantonov <ajantonov@yahoo.com>
2016-09-05 16:15:44 +0300

    Add example for use of super(...).

--- class/inheritance/Animal.java
+++ class/inheritance/Animal.java
@@ -1,6 +1,12 @@
 public class Animal {
     private int age;
     
+    public Animal(int age) {
+        super();
+
+        this.age = age;
+    }
+
     public void setAge(int age) {
         if (age > 0) 
             this.age = age;
