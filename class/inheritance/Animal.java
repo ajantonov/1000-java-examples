09b6bd9574b9b4545b931363b9ceeba374cc965d
ajantonov <ajantonov@yahoo.com>
2016-09-05 16:25:06 +0300

    Add example for use of super(...) when are available more parent constructors.

--- class/inheritance/Animal.java
+++ class/inheritance/Animal.java
@@ -1,10 +1,18 @@
 public class Animal {
-    private int age;
+    private int     age;
+    private String  name;
     
+    public Animal(int age, String name) {
+        super();
+        this.age = age;
+        this.name = name;
+    }
+
     public Animal(int age) {
         super();
 
         this.age = age;
+        this.name = null;
     }
 
     public void setAge(int age) {
---
+++ class/inheritance/Gorilla.java
@@ -0,0 +1,11 @@
+public class Gorilla extends Animal {
+    
+    public Gorilla() {
+        super(5);
+    }
+
+    public Gorilla(int age) {
+        super(age, "Gorilla");
+    }
+    
+}
