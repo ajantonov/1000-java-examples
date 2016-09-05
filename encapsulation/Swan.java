6705b11ff4eedc36740d83a347248c409c19d079
ajantonov <ajantonov@yahoo.com>
2016-09-05 11:35:04 +0300

    Add example for Encapsulation.

---
+++ encapsulation/Swan.java
@@ -0,0 +1,11 @@
+public class Swan {
+    
+    private int numberEggs;
+    public int getNumberEggs() {
+        return numberEggs;
+    }
+    public void setNumberEggs(int numberEggs) {
+        if (numberEggs > 0)
+            this.numberEggs = numberEggs;
+    }  
+}  
