9539c0819c58456e3108c4a0e4a49bca8b4a906d
ajantonov <ajantonov@yahoo.com>
2016-09-07 20:37:58 +0300

    Add example that you can't override static method with instance method and you can't hide instance method with static method.

---
+++ class/inheritance/BearB.java
@@ -0,0 +1,9 @@
+public class BearB {
+    public static void sneeze() {
+        System.out.println("Bear is sneezing");
+    }
+
+    public void hibernate() {
+        System.out.println("Bear is hibernating");
+    }
+}
---
+++ class/inheritance/PandaB.java
@@ -0,0 +1,10 @@
+public class Panda extends BearB {
+    
+    public void sneeze() {
+        System.out.println("Panda bear sneeze quitely");
+    }
+
+    public static void hibernate() {
+        System.out.println("Panda bear is goind to sleep");
+    }
+}
