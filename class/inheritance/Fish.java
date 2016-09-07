ee62cafb2cd5cbf331bb345c7de48f9bd28a97eb
ajantonov <ajantonov@yahoo.com>
2016-09-07 15:12:25 +0300

    Add example for class inheritance.

---
+++ class/inheritance/Fish.java
@@ -0,0 +1,12 @@
+public class Fish {
+    private     int age;
+    protected   int size;
+
+    public Fish(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
