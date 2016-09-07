6486043486814f01399db2d30e2be8da72c7df6b
ajantonov <ajantonov@yahoo.com>
2016-09-07 20:03:58 +0300

    Add example for static method hiding.

---
+++ class/inheritance/Bear.java
@@ -0,0 +1,5 @@
+public class Bear {
+    public static void eat() {
+        System.out.println("Bear eat");
+    }
+}
---
+++ class/inheritance/Panda.java
@@ -0,0 +1,9 @@
+public class Panda extends Bear {
+    public static void eat() {
+        System.out.println("Panda is chewing");
+    }
+
+    public static void main(String args[]) {
+        Panda.eat();        
+    }
+}
