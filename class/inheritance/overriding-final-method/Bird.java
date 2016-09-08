48880034b7a842f198bd4ea9da29da6db9e5fb00
ajantonov <ajantonov@yahoo.com>
2016-09-08 13:36:31 +0300

    Add example that you can't override final method.

---
+++ class/inheritance/overriding-final-method/Bird.java
@@ -0,0 +1,5 @@
+public class Bird {
+    public final boolean hasFeathers() {
+        return true;
+    }
+}
---
+++ class/inheritance/overriding-final-method/Penguin.java
@@ -0,0 +1,5 @@
+public class Penguin extends Bird {
+    public final boolean hasFeathers() {
+        return true;
+    }
+}
