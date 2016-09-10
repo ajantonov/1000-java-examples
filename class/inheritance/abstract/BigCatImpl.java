c13f2166ebe751ce721dc1d2950392add3e2e149
ajantonov <ajantonov@yahoo.com>
2016-09-10 12:01:20 +0300

    Add exception of the rule that you have to implement abstract method.

---
+++ class/inheritance/abstract/BigCatImpl.java
@@ -0,0 +1,6 @@
+public abstract class BigCatImpl extends Animal {
+    public String getName() {
+        return "BigCatImpl";
+    }
+    public abstract void roar();
+}
---
+++ class/inheritance/abstract/LionImpl.java
@@ -0,0 +1,5 @@
+public class LionImpl extends BigCatImpl {
+    public void roar() {
+        System.out.println("The Lion lets out a loud ROAR!");
+    }
+}
