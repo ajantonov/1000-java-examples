a6e013cc7ecd81ce6fb4c37320313f1635484c77
ajantonov <ajantonov@yahoo.com>
2016-09-12 15:09:02 +0300

    Add example for interface variables with all access modifiers and specific options as compiler would do.

---
+++ interface/CanSwim.java
@@ -0,0 +1,5 @@
+public abstract interface CanSwim {
+    public static final int MAXIMUM_DEPTH = 100;
+    public static final boolean UNDERWATER = true;
+    public static final String TYPE = "Submersible";
+}
