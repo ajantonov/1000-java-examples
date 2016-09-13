67b7d7fbf65f0c30006bdbd60b4337177aa57a72
ajantonov <ajantonov@yahoo.com>
2016-09-13 15:58:35 +0300

    Add 'example' how you cant't cast from superclass to subclass.

---
+++ class/inheritance/polymorphism/Capybara.java
@@ -0,0 +1,7 @@
+public class Capybara extends Rodent {
+    
+    public static void main(String[] args) {
+        Rodent rodent = new Rodent();
+        Capybara capybara = (Capybara)rodent;
+    }
+}
