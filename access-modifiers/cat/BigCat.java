e840441e7ee3dda6444d1aa1eeaef6631f1c3a11
ajantonov <ajantonov@yahoo.com>
2016-11-04 20:10:15 +0200

    Added example for access modifiers from OCP Java SE 8

---
+++ access-modifiers/cat/BigCat.java
@@ -0,0 +1,10 @@
+package cat;
+
+public class BigCat{
+
+    public      String name = "cat";
+    protected   boolean hasFur = true;
+                boolean hasPaws = true;
+    private     int     id;
+
+}
