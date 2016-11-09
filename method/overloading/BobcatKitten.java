bb78e8ec8798ba4b894ef6c809473849d2ff108b
ajantonov <ajantonov@yahoo.com>
2016-11-09 23:54:53 +0200

    [OCP] Added example for overloading and overriding.

---
+++ method/overloading/BobcatKitten.java
@@ -0,0 +1,13 @@
+public class BobcatKitten extends Bobcat {
+    public void findDen() {
+    
+    }    
+    
+    public void findDen(boolean b) {
+    
+    }
+    
+    public int findden() throws Exception {
+        return 0;
+    }
+}
